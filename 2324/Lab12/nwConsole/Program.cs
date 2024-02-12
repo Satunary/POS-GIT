6395ad34f8ae12a02a84b5829c5fafacb39fc531
Satunary <max.augsten@ah-solutions.at>
2024-02-12 10:07:55 +0100

    a

--- 2324/Lab12/nwConsole/Program.cs
+++ 2324/Lab12/nwConsole/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using nwConsole.Model;
+
 Console.WriteLine("Hello, World!");
+using (var db = new Northwind4SqliteContext())
+{
+     var erg1 = db.Employees.Where(e => e.Title == "Vice President, Sales").Select(e=>e.FirstName);
+     foreach (var item in erg1)
+     {
+         Console.WriteLine(item);
+     }
+    
+
+ 
+}
