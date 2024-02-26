2c73f15cee5e6e53f9fb401812fbc37f4979ce37
Satunary <max.augsten@ah-solutions.at>
2024-02-26 14:16:20 +0100

    lab14

--- 2324/Lab14/MainPage.xaml.cs
+++ 2324/Lab14/MainPage.xaml.cs
@@ -6,9 +6,9 @@ namespace Lab14
 {
     public partial class MainPage : ContentPage
     {
-        
+        private bool tryme = false;
         XmlTextReader reader = new XmlTextReader("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
-        Dictionary<string,int> prices = new Dictionary<string,int>();
+        Dictionary<string,double> prices = new Dictionary<string,double>();
 
         public MainPage()
         {
@@ -22,14 +22,48 @@ namespace Lab14
             {
                 switch(reader.NodeType)
                 {
-                    case XmlNodeType.Attribute:
-                        //
-                        //reader.Value;
+                    case XmlNodeType.Element:
+                        if (reader.Name == "Cube" && reader.GetAttribute("currency") != null && reader.GetAttribute("rate") != null)
+                        {
+                            prices.Add(reader.GetAttribute("currency"), double.Parse(reader.GetAttribute("rate").Replace(".",",")));
+                        }
                         break;
                 }
             }
+
+            foreach (var item in prices)
+            {
+                pickme.Items.Add(item.Key);   
+            }
         }
-       
+
+        private void pickme_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnAnd.Text = pickme.SelectedItem.ToString()+" => Euro";
+            btnEur.Text = "Euro => "+pickme.SelectedItem.ToString();
+           sum.Text="Summe in "+ pickme.SelectedItem.ToString();
+            
+        }
+
+        private void valEur_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            if (valEur.Text != null && pickme.SelectedItem != null)
+            {
+                valAnd.Text = (double.Parse(valEur.Text) * prices[pickme.SelectedItem.ToString()]).ToString();
+               
+            }
+        }
+
+        private void valAnd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            if ( valAnd.Text != null&& double.TryParse(valAnd.Text,out double a) && pickme.SelectedItem != null)
+            {
+                valEur.Text = (double.Parse(valAnd.Text) / prices[pickme.SelectedItem.ToString()]).ToString();
+                
+            }
+        }
     }
 
 }
