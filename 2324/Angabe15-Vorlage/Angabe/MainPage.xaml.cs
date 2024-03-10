125fa49e81762e263ed8d81fa6c644a3e68bb6d9
Augsten-Spenger <max.augsten@ah-solutions.at>
2024-03-10 19:16:54 +0100

    17

--- 2324/Angabe15-Vorlage/Angabe/MainPage.xaml.cs
+++ 2324/Angabe15-Vorlage/Angabe/MainPage.xaml.cs
@@ -1,17 +1,18 @@
-﻿namespace Angabe {
-    public partial class MainPage : ContentPage {
-        int count = 0;
+﻿using System.Collections.ObjectModel;
 
+namespace Angabe {
+    public partial class MainPage : ContentPage {
+        
+        private ObservableCollection<WeirdThing> weirdThings=new();
         public MainPage()
         {
             InitializeComponent();
+            col.ItemsSource = weirdThings;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Search(object sender, EventArgs e)
         {
-            count++;
-
-            
+            weirdThings.Add(new WeirdThing() { Bez=entry1.Text});
         }
     }
 
---
+++ 2324/Angabe15-Vorlage/Angabe/WeirdThing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angabe
+{
+    public class WeirdThing
+    {
+        public string Bez { get; set; }
+    }
+}
