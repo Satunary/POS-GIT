806f780b9ca0f267becb028bd84ab9e153d22c8f
Satunary <max.augsten@ah-solutions.at>
2024-04-16 13:00:30 +0200

    aaaaaa

---
+++ 2324/Project24/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Project24
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
